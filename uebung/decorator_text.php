49cca84a292eb60d5d1c8dce53062f7f4affccbb
011s01 (Schulungsuser01 in Raum011) <011s01@itaf.it.nrw.de>
2025-11-12 15:02:02 +0100

    decorater

---
+++ uebung/decorator_text.php
@@ -0,0 +1,57 @@
+<?php
+
+interface Text{
+
+    public function render(string $text):string;
+}
+
+class PlainText implements Text{
+
+    public function render(string $text):String{
+        return $text;
+    }
+}
+
+abstract class TextDecorator implements Text{
+    public function __construct(protected Text $text)
+    {
+    }
+
+    public function render(string $text):string
+    {
+        return $this->text->render($text);
+    }
+}
+
+class BoldText extends TextDecorator
+{
+    public function render(string $text): string
+    {
+        return "<b>". parent::render($text). "</b>";
+
+    }
+}
+
+class ItalicText extends TextDecorator{
+    public function render(string $text):string{
+        return "<i>" . parent::render($text) . "</i>";
+    }
+
+}
+
+class UnderlineText extends TextDecorator{
+    public function render(string $text):string{
+        return "<i>". parent::render($text) . "</i>";
+    }
+}
+
+$underline = new UnderlineText(
+    new ItalicText(
+        new BoldText(
+            new PlainText()
+        )
+    )
+);
+
+echo $underline->render("Test");
+
